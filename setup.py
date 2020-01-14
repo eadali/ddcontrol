36b593fe5bc33ff85fc6c9901ff4f9183159f69e
eadali <erkanadali91@gmail.com>
2020-01-14 16:19:43 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 14 15:05:54 2020
+
+@author: ERADALI
+"""
+
+from distutils.core import setup
+setup(
+  name = 'ddcontrol',
+  packages = ['ddcontrol'],
+  version = '0.1',
+  license='MIT',
+  description = 'The design of the controller are based entirely on experimental data collected from the plant',
+  author = 'Erkan ADALI',
+  author_email = 'erkanadali91@gmail.com',
+  url = 'https://github.com/eadali/ddcontrol',
+  download_url = 'https://github.com/eadali/ddcontrol/archive/v0.1.tar.gz',
+  keywords = ['control', 'pid', 'tune', 'real time',],
+  install_requires=['numpy', 'scipy',],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
