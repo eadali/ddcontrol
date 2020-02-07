27c8c6daabbcd9aec3b2e843240794593f22c767
eadali <erkanadali91@gmail.com>
2020-02-07 21:48:52 +0300

    on wokr

--- ddcontrol/control.py
+++ ddcontrol/control.py
@@ -10,110 +10,109 @@ from warnings import warn
 from threading import Thread, Event
 
 
+
 class PIDController(Thread):
     def __init__(self, kp, ki, kd, kn, freq=10.0, lmin=-float('Inf'), lmax=float('Inf')):
-        """Inits PIDContoller parameters
-        #Arguments
-            kp: Proportional gain
-            ki: Integral gain
-            kd: Derivative gain
+        """Advenced PID controller interface
+        
+        Args:
+            kp (float): Proportional gain of controller
+            ki (float): Integral gain of controller
+            kd (float): Derivative gain of controller
+            kn (float):
+            freq(float, optional): 
+            lmin, lmax (float, optional):
         """
         Thread.__init__(self)
         #Stop event for thread
-        self.stop_event = Event()
+        self.sflag = Event()
         # Gains
         self.kp, self.ki, self.kd, self.kn = kp, ki, kd, kn
         #Other parameters
         self.freq, self.lmin, self.lmax = freq, lmin, lmax
         #Integral value of integral term
         #Integral value of derivative term
-        self.integral, self.filt_integral = 0.0, 0.0
+        self.int, self.fint = 0.0, 0.0
         #Error signal value
         #Control signal value
-        self.err, self.u_control = 0.0, 0.0
+        self.e, self.u = 0.0, 0.0
         #Previously measured timestamp
-        self.past = None
+        self.ptime = None
         # Anti-Wind Up status
         self.windup = False
+        
+        
+    def __sign(self, x):
+        if x < 0.0:
+            return -1.0
+        elif x > 0.0:
+            return 1.0
+        return 0.0
     
     
-    def sign(self,value):
-        """Sign function
-        #Arguments:
-            value: value for sign detection
-        #Returns:
-            Sign of value
+    def __clip(self, x, lmin, lmax):
+        if x < lmin:
+            return lmin
+        elif x > lmax:
+            return lmax
+        return x
+    
+    
+    def __isclose(self, x1, x2, rtol=1e-15):
+        return abs(x1-x2) < rtol
+        
+
+    def step(self, e, dt):
+        #Calculates proportional term
+        up = self.kp * e
+        # Calculates integral term
+        if not self.windup:
+            self.int += self.ki * e * dt
+        ui = self.int
+        # Calculates derivative term
+        ud = self.kn * (self.kd * e - self.fint)
+        self.fint += ud * dt
+        #Anti-windup
+        u = up + ui + ud
+        self.windup = False
+        if u < self.lmin or u > self.lmax:
+            if self.__isclose(self.__sign(e),self.__sign(u)):
+                self.windup = True
+        # Calculates output
+        self.u =  self.__clip(u, self.lmin, self.lmax)
+        return self.u
+
+
+    def update(self, e):
+        """Updates error value of PIDController.
+        
+        Args:
+            e (float): Error signal value
+        
+        Returns:
+            float: Control signal value
         """
-        sgn = 0.0
-        if value < 0.0:
-            sgn = -1.0
-        elif value > 0.0:
-            sgn = 1.0
-        return sgn
+        self.e = e
+        return self.u
 
 
-    def clip(self, value, lmin, lmax):
-        """Clip function
-        #Arguments:
-            value: value for clip
-            lmin: Minimum limit
-            lmax: Maximum limit
-        #Returns:
-            Clipped value
-        """
-        clip_val = value
-        if value > lmax:
-            clip_val = lmax
-        elif value < lmin:
-            clip_val = lmin
-        return clip_val
-
-
-    def update(self, err):
-        """Update function for PID controller
-        #Arguments
-            err: Error signal value
-        #Returns
-            Value of control signal
-        """        
-        self.err = err
-        return self.u_control
-    
-    
     def run(self):
-        """Loop for thread
-        """
+        #Loop for thread
         self.stop_event.clear()
         while not self.stop_event.is_set():
             #Fixes frequency of controller
-            if self.past is not None:
-                wait = (1.0/self.freq) - (time()-self.past)
+            if self.ptime is not None:
+                wait = (1.0/self.freq) - (time()-self.ptime)
                 if wait > 0.0:
                     sleep(wait)
                 else:
-                    warn('PID contoller frequency is lower than \'freq\' value', RuntimeWarning)
-            self.past = time()
-    
-            #Calculates proportional term
-            u_proportional = self.kp * self.err
-            # Calculates integral term
-            if not self.windup:
-                self.integral += self.ki * self.err * (1.0/self.freq)
-            u_integral = self.integral
-            # Calculates derivative term
-            u_derivative = self.kn * (self.kd * self.err - self.filt_integral)
-            self.filt_integral += u_derivative * (1/self.freq)
-            #Anti-windup
-            u_sum = u_proportional + u_integral + u_derivative
-            self.windup = False
-            if u_sum < self.lmin or u_sum > self.lmax:
-                if abs(self.sign(self.err) - self.sign(u_sum)) < 1e-15:
-                    self.windup = True
-            # Calculates output
-            self.u_control =  self.clip(u_sum, self.lmin, self.lmax)
-        
-    
+                    warn('PID contoller frequency is lower than \'freq\' value.', RuntimeWarning)
+            self.ptime = time()
+            self.step(self.e, (1.0/self.freq))
+
+
     def stop(self):
         """Stops thread
         """
+        warn('bele vaziyyetin icine soxum.', RuntimeWarning)
         self.stop_event.set()
--- test_ddcontrol.py
+++ test_ddcontrol.py
@@ -13,167 +13,168 @@ from scipy.signal import lsim2, lti
 from time import time, sleep
 
 
-#def test_CInterp1d():
-#    """Test of CInterp1d Class
-#    """
-#    f = CInterp1d(lambda x: [0.0], 0.0)
-#    f.append(1.0, [1.0])
-#    check1 = absolute(f(0.0)) < 1e-4
-#    check2 = absolute(f(0.5)-0.5) < 1e-4
-#    check3 = absolute(f(1.5)-1.5) < 1e-4
-#    assert check1 and check2 and check3
-#
-#
-#def test_DDE():
-#    """Test of DDE Class
-#    """
-#    f = lambda t, x: [-x(t-1.0)]
-#    solver = DDE(f)
-#    solver.set_integrator('dopri5')
-#    g = lambda t: [1.0]
-#    t = linspace(0,10,101)
-#    y = zeros(t.shape)
-#    solver.set_initial_value(g, 0.0)
-#    for index in range(t.shape[0]):
-#        y[index] = solver.integrate(t[index])
-#    check1 = absolute(y[0]-1.0) < 0.01
-#    check2 = (absolute(y[-10:]) < 0.1).all()
-#    assert check1 and check2
-#
-#
-#def test_StateSpace():
-#    """Test of StateSpace Class
-#    """
-#    t = linspace(0,10,101)
-#    u = ones(t.shape)
-#    A, B = [[0.0, 1.0], [0.0, 0.0]], [[0.0], [1.0]]
-#    C, D = [[1.0, 0.0]], [[0.0]]
-#    scipy_ss = lti(A, B, C, D)
-#    _, y_scipy, _ = lsim2(scipy_ss, u, t)
-#    ss = StateSpace(A, B, C, D)
-#    y_ss = zeros(t.shape)
-#    for index in range(t.shape[0]):
-#        y_ss[index] = ss.integrate(t[index], u[index])
-#    assert (absolute(y_ss-y_scipy) < 0.1).all()
-#
-#
-#def test_StateSpace_udelay():
-#    """Test of StateSpace Class with input delay
-#    """
-#    t = linspace(0,10,101)
-#    u = ones(t.shape)
-#    A, B = [[0.0, 1.0], [0.0, 0.0]], [[0.0], [1.0]]
-#    C, D = [[1.0, 0.0]], [[0.0]]
-#    scipy_ss = lti(A, B, C, D)
-#    _, y_scipy, _ = lsim2(scipy_ss, u, t)
-#    A = [[[0.0, 1.0], [0.0, 0.0]], [[0.0, 0.0], [0.0, 0.0]]]
-#    B = [[[0.0], [0.0]], [[0.0], [1.0]]]
-#    C = [[[1.0, 0.0]], [[0.0, 0.0]]]
-#    D = [[[0.0]]]
-#    ss = StateSpace(A, B, C, D, delays=[0.0,1.0])
-#    y_ss = zeros(t.shape)
-#    for index in range(t.shape[0]):
-#        y_ss[index] = ss.integrate(t[index], u[index])
-#    assert (absolute(y_ss[argmax(t>=1.0):]-y_scipy[:-argmax(t>=1.0)] < 0.1).all())
-#
-#
-#def test_StateSpace_ydelay():
-#    """Test of StateSpace Class with output delay
-#    """
-#    t = linspace(0,10,101)
-#    u = ones(t.shape)
-#    A, B = [[0.0, 1.0], [0.0, 0.0]], [[0.0], [1.0]]
-#    C, D = [[1.0, 0.0]], [[0.0]]
-#    scipy_ss = lti(A, B, C, D)
-#    _, y_scipy, _ = lsim2(scipy_ss, u, t)
-#    A = [[[0.0, 1.0], [0.0, 0.0]], [[0.0, 0.0], [0.0, 0.0]]]
-#    B = [[[0.0], [1.0]], [[0.0], [0.0]]]
-#    C = [[[0., 0.]], [[1.0, 0.0]]]
-#    D = [[[0.0]]]
-#    ss = StateSpace(A, B, C, D, delays=[0.0,1.0])
-#    y_ss = zeros(t.shape)
-#    for index in range(t.shape[0]):
-#        y_ss[index] = ss.integrate(t[index], u[index])
-#    assert (absolute(y_ss[argmax(t>=1.0):]-y_scipy[:-argmax(t>=1.0)] < 0.1).all())
-#
-#
-#def test_StateSpace_sdelay():
-#    """Test of StateSpace Class with state delay
-#    """
-#    t = linspace(0,10,101)
-#    f = lambda t, x: [-x(t-1.0)]
-#    solver = DDE(f)
-#    solver.set_integrator('dopri5')
-#    g = lambda t: [1.0]
-#    y_dde = zeros(t.shape)
-#    solver.set_initial_value(g, 0.0)
-#    for index in range(t.shape[0]):
-#        y_dde[index] = solver.integrate(t[index])
-#    A = [[[0.0]], [[-1.0]]]
-#    B = [[[0.0]], [[0.0]]]
-#    C = [[[1.0]],[[0.0]]]
-#    D = [[[0.0]],[[0.0]]]
-#    ss = StateSpace(A, B, C, D, delays=[0.0,1.0])
-#    ss.set_initial_value(x0=[1.0])
-#    y_ss = zeros(t.shape)
-#    for index in range(t.shape[0]):
-#        y_ss[index] = ss.integrate(t[index], [0.0])
-#    assert (absolute(y_ss-y_dde) < 0.1).all()
-#
-#
-#def test_TransferFunction():
-#    """Test of TransferFunction Class
-#    """
-#    t = linspace(0,10,101)
-#    u = ones(t.shape)
-#    num = [1.0, 3.0, 3.0]
-#    den = [1.0, 2.0, 1.0]
-#    tf = TransferFunction(num, den)
-#    scipy_tf = lti(num, den)
-#    _, y_scipy, _ = lsim2(scipy_tf, u, t)
-#    y_tf = zeros(t.shape)
-#    for index in range(t.shape[0]):
-#        y_tf[index] = tf.integrate(t[index], u[index])
-#    assert (absolute(y_tf-y_scipy)<0.1).all()
-#
-#
-#def test_TransferFunction_udelay():
-#    """Test of TransferFunction Class with input delay
-#    """
-#    t = linspace(0,10,101)
-#    u = ones(t.shape)
-#    num = [1.0, 3.0, 3.0]
-#    den = [1.0, 2.0, 1.0]
-#    tf = TransferFunction(num, den)
-#    scipy_tf = lti(num, den)
-#    _, y_scipy, _ = lsim2(scipy_tf, u, t)
-#    tf = TransferFunction(num, den, 1.0)
-#    tf.set_initial_value(0.0)
-#    y_tf = zeros(t.shape)
-#    for index in range(t.shape[0]):
-#        y_tf[index] = tf.integrate(t[index], u[index])
-#    assert (absolute(y_tf[argmax(t>=1.0):]-y_scipy[:-argmax(t>=1.0)] < 0.1).all())
-#
-#
-#def test_tfest():
-#    """Test of tfest Method
-#    """
-#    t = linspace(0,10,101)
-#    u = ones(t.shape)
-#    num = [1.0, 3.0, 3.0]
-#    den = [1.0, 2.0, 1.0]
-#    scipy_tf = lti(num, den)
-#    _, y_scipy, _ = lsim2(scipy_tf, u, t)
-#    tf, _ = tfest(t, y_scipy, u, 3, 3)
-#    y_tf = zeros(t.shape)
-#    for index in range(t.shape[0]):
-#        y_tf[index] = tf.integrate(t[index], u[index])
-#    assert (absolute(y_tf-y_scipy)<0.1).all()
-#    
-#    
-#def test_tfest_udelay():
-#    """Test of tfest Method with udelay
-#    """
+def test_CInterp1d():
+    """Test of CInterp1d Class
+    """
+    f = CInterp1d(lambda x: [0.0], 0.0)
+    f.append(1.0, [1.0])
+    check1 = absolute(f(0.0)) < 1e-4
+    check2 = absolute(f(0.5)-0.5) < 1e-4
+    check3 = absolute(f(1.5)-1.5) < 1e-4
+    assert check1 and check2 and check3
+
+
+def test_DDE():
+    """Test of DDE Class
+    """
+    f = lambda t, x: [-x(t-1.0)]
+    solver = DDE(f)
+    solver.set_integrator('dopri5')
+    g = lambda t: [1.0]
+    t = linspace(0,10,101)
+    y = zeros(t.shape)
+    solver.set_initial_value(g, 0.0)
+    for index in range(t.shape[0]):
+        y[index] = solver.integrate(t[index])
+    check1 = absolute(y[0]-1.0) < 0.01
+    check2 = (absolute(y[-10:]) < 0.1).all()
+    assert check1 and check2
+
+
+def test_StateSpace():
+    """Test of StateSpace Class
+    """
+    t = linspace(0,10,101)
+    u = ones(t.shape)
+    A, B = [[0.0, 1.0], [0.0, 0.0]], [[0.0], [1.0]]
+    C, D = [[1.0, 0.0]], [[0.0]]
+    scipy_ss = lti(A, B, C, D)
+    _, y_scipy, _ = lsim2(scipy_ss, u, t)
+    ss = StateSpace(A, B, C, D)
+    y_ss = zeros(t.shape)
+    for index in range(t.shape[0]):
+        y_ss[index] = ss.integrate(t[index], u[index])
+    assert (absolute(y_ss-y_scipy) < 0.1).all()
+
+
+def test_StateSpace_udelay():
+    """Test of StateSpace Class with input delay
+    """
+    t = linspace(0,10,101)
+    u = ones(t.shape)
+    A, B = [[0.0, 1.0], [0.0, 0.0]], [[0.0], [1.0]]
+    C, D = [[1.0, 0.0]], [[0.0]]
+    scipy_ss = lti(A, B, C, D)
+    _, y_scipy, _ = lsim2(scipy_ss, u, t)
+    A = [[[0.0, 1.0], [0.0, 0.0]], [[0.0, 0.0], [0.0, 0.0]]]
+    B = [[[0.0], [0.0]], [[0.0], [1.0]]]
+    C = [[[1.0, 0.0]], [[0.0, 0.0]]]
+    D = [[[0.0]]]
+    ss = StateSpace(A, B, C, D, delays=[0.0,1.0])
+    y_ss = zeros(t.shape)
+    for index in range(t.shape[0]):
+        y_ss[index] = ss.integrate(t[index], u[index])
+    assert (absolute(y_ss[argmax(t>=1.0):]-y_scipy[:-argmax(t>=1.0)] < 0.1).all())
+
+
+def test_StateSpace_ydelay():
+    """Test of StateSpace Class with output delay
+    """
+    t = linspace(0,10,101)
+    u = ones(t.shape)
+    A, B = [[0.0, 1.0], [0.0, 0.0]], [[0.0], [1.0]]
+    C, D = [[1.0, 0.0]], [[0.0]]
+    scipy_ss = lti(A, B, C, D)
+    _, y_scipy, _ = lsim2(scipy_ss, u, t)
+    A = [[[0.0, 1.0], [0.0, 0.0]], [[0.0, 0.0], [0.0, 0.0]]]
+    B = [[[0.0], [1.0]], [[0.0], [0.0]]]
+    C = [[[0., 0.]], [[1.0, 0.0]]]
+    D = [[[0.0]]]
+    ss = StateSpace(A, B, C, D, delays=[0.0,1.0])
+    y_ss = zeros(t.shape)
+    for index in range(t.shape[0]):
+        y_ss[index] = ss.integrate(t[index], u[index])
+    assert (absolute(y_ss[argmax(t>=1.0):]-y_scipy[:-argmax(t>=1.0)] < 0.1).all())
+
+
+def test_StateSpace_sdelay():
+    """Test of StateSpace Class with state delay
+    """
+    t = linspace(0,10,101)
+    f = lambda t, x: [-x(t-1.0)]
+    solver = DDE(f)
+    solver.set_integrator('dopri5')
+    g = lambda t: [1.0]
+    y_dde = zeros(t.shape)
+    solver.set_initial_value(g, 0.0)
+    for index in range(t.shape[0]):
+        y_dde[index] = solver.integrate(t[index])
+    A = [[[0.0]], [[-1.0]]]
+    B = [[[0.0]], [[0.0]]]
+    C = [[[1.0]],[[0.0]]]
+    D = [[[0.0]],[[0.0]]]
+    ss = StateSpace(A, B, C, D, delays=[0.0,1.0])
+    ss.set_initial_value(x0=[1.0])
+    y_ss = zeros(t.shape)
+    for index in range(t.shape[0]):
+        y_ss[index] = ss.integrate(t[index], [0.0])
+    assert (absolute(y_ss-y_dde) < 0.1).all()
+
+
+def test_TransferFunction():
+    """Test of TransferFunction Class
+    """
+    t = linspace(0,10,101)
+    u = ones(t.shape)
+    num = [1.0, 3.0, 3.0]
+    den = [1.0, 2.0, 1.0]
+    tf = TransferFunction(num, den)
+    scipy_tf = lti(num, den)
+    _, y_scipy, _ = lsim2(scipy_tf, u, t)
+    y_tf = zeros(t.shape)
+    for index in range(t.shape[0]):
+        y_tf[index] = tf.integrate(t[index], u[index])
+    assert (absolute(y_tf-y_scipy)<0.1).all()
+
+
+def test_TransferFunction_udelay():
+    """Test of TransferFunction Class with input delay
+    """
+    t = linspace(0,10,101)
+    u = ones(t.shape)
+    num = [1.0, 3.0, 3.0]
+    den = [1.0, 2.0, 1.0]
+    tf = TransferFunction(num, den)
+    scipy_tf = lti(num, den)
+    _, y_scipy, _ = lsim2(scipy_tf, u, t)
+    tf = TransferFunction(num, den, 1.0)
+    tf.set_initial_value(0.0)
+    y_tf = zeros(t.shape)
+    for index in range(t.shape[0]):
+        y_tf[index] = tf.integrate(t[index], u[index])
+    assert (absolute(y_tf[argmax(t>=1.0):]-y_scipy[:-argmax(t>=1.0)] < 0.1).all())
+
+
+def test_tfest():
+    """Test of tfest Method
+    """
+    t = linspace(0,10,101)
+    u = ones(t.shape)
+    num = [1.0, 3.0, 3.0]
+    den = [1.0, 2.0, 1.0]
+    scipy_tf = lti(num, den)
+    _, y_scipy, _ = lsim2(scipy_tf, u, t)
+    tf, _ = tfest(t, y_scipy, u, 3, 3)
+    y_tf = zeros(t.shape)
+    for index in range(t.shape[0]):
+        y_tf[index] = tf.integrate(t[index], u[index])
+    assert (absolute(y_tf-y_scipy)<0.1).all()
+    
+    
+def test_tfest_udelay():
+    """Test of tfest Method with udelay
+    """
+    assert False
 #    t = linspace(0,10,101)
 #    u = ones(t.shape)
 #    num = [1.0, 3.0, 3.0]
@@ -186,82 +187,53 @@ from time import time, sleep
 #    y_tf = zeros(t.shape)
 #    for index in range(t.shape[0]):
 #        y_tf[index] = tf.integrate(t[index], u[index])
-#    from matplotlib import pyplot
-#    pyplot.plot(y_scipy)
-#    pyplot.plot(y_tf)
-#    pyplot.show()
-#    assert False
+##    from matplotlib import pyplot
+##    pyplot.plot(y_scipy)
+##    pyplot.plot(y_tf)
+##    pyplot.show()
 #    assert (absolute(y_tf-y_scipy)<0.1).all()
     
 
 def test_PIDController_P():
     """Test of PIDController Class P gain
     """
-    pid = PIDController(kp=1.0, ki=0.0, kd=0.0, kn=0.0, freq=10.0)
+    pid = PIDController(kp=1.0, ki=0.0, kd=0.0, kn=0.0)
+    u = pid.step(1.0, 1.0)
+    assert abs(u-1.0) < 1e-4
+
+
+def test_PIDController_I():
+    """Test of PIDController Class I gain
+    """
+    pid = PIDController(kp=0.0, ki=1.0, kd=0.0, kn=0.0)
+    u = pid.step(1.0, 1.0)
+    assert abs(u-1.0) < 1e-4
+
+
+def test_PIDController_D():
+    """Test of PIDController D gain
+    """
+    pid = PIDController(kp=0.0, ki=0.0, kd=1.0, kn=1.0, freq=10.0)
+    u = pid.step(1.0, 1.0)
+    assert abs(u-1.0) < 1e-4
+
+
+def test_PIDController_clamp():
+    """Test of PIDController clamp
+    """
+    pid = PIDController(kp=0.0, ki=1.0, kd=0.0, kn=0.0, freq=10.0, lmin=-0.4, lmax=0.4)
     pid.start()
-    pid.integrate(1.0)
-    sleep(0.4)
-    u = pid.integrate(1.0)
+    pid.update(1.0)
+    sleep(1.0)
+    u_cont = pid.update(1.0)
+    check1 = u_cont < 0.5
+    pid.update(-1.0)
+    sleep(1.0)
+    u_cont = pid.update(-1.0)
+    check2 = u_cont > -0.5
     pid.stop()
     pid.join()
-    assert abs(1.0-u) < 0.001
-#
-#
-#def test_PIDController_I():
-#    """Test of PIDController I gain
-#    """
-#    pid = PIDController(kp=0.0, ki=1.0, kd=0.0, kn=0.0, freq=10.0)
-#    pid.start()
-#    pid.update(1.0)
-#    sleep(1.0)
-#    u_cont = pid.update(1.0)
-#    pid.stop()
-#    pid.join()
-#    assert abs(1.0-u_cont) < 0.2
-#
-#
-#def test_PIDController_D():
-#    """Test of PIDController D gain
-#    """
-#    pid = PIDController(kp=0.0, ki=0.0, kd=1.0, kn=1.0, freq=10.0)
-#    pid.start()
-#    pid.update(1.0)
-#    sleep(1.0)
-#    u_cont = pid.update(1.0)
-#    pid.stop()
-#    pid.join()
-#    assert abs(0.4-u_cont) < 0.2
-#
-#
-#def test_PIDController_freq():
-#    """Test of PIDController frequency
-#    """
-#    pid = PIDController(kp=0.0, ki=1.0, kd=0.0, kn=0.0, freq=10.0)
-#    pid.start()
-#    start = time()
-#    sleep(1.0)
-#    end = pid.past
-#    pid.stop()
-#    pid.join()
-#    assert abs(end-start-1.0) < 0.2
-#
-#
-#def test_PIDController_clamp():
-#    """Test of PIDController clamp
-#    """
-#    pid = PIDController(kp=0.0, ki=1.0, kd=0.0, kn=0.0, freq=10.0, lmin=-0.4, lmax=0.4)
-#    pid.start()
-#    pid.update(1.0)
-#    sleep(1.0)
-#    u_cont = pid.update(1.0)
-#    check1 = u_cont < 0.5
-#    pid.update(-1.0)
-#    sleep(1.0)
-#    u_cont = pid.update(-1.0)
-#    check2 = u_cont > -0.5
-#    pid.stop()
-#    pid.join()
-#    assert check1 and check2
+    assert check1 and check2
 #
 #
 #def test_PIDController():
