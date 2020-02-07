737012d1d36862cba8fecb27fddd6fa9057d3755
eadali <erkanadali91@gmail.com>
2020-02-07 15:16:41 +0300

    Update model.py

--- ddcontrol/model.py
+++ ddcontrol/model.py
@@ -204,57 +204,6 @@ def tfest(t, y, u, np, nz=None, delay=False, epsfcn=1e-5):
     return (TransferFunction(num, den, udelay), pcov)
 
 
-##TODO: Reframe model
-class MSDModel:
-    def __init__(self, m, k, b, x0):
-        """Inits mass-spring-damper model
-        #Arguments:
-            m: Mass value
-            k: Spring value
-            b: Damper value
-            x0: Initial states
-        """
-        #System Parameters
-        self.m, self.k, self.b = m, k, b
-        #Initial states
-        self.x0 = x0
-        #Previously measured timestamp
-        self.past = None
-
-
-    def ode(self, x, t, u):
-        """Dynamic equations of mass-spring-damper
-        #Arguments:
-            x: States of ode
-            t: Timestamps
-            u: Control signal value
-        #Returns
-            Derivative of states
-        """
-        # ODE of pendulum
-        pos, vel = x
-        dxdt = [vel, -(self.k/self.m)*pos - (self.b/self.m)*vel + (1.0/self.m)*u]
-        return dxdt
-
-
-    def update(self, u):
-        """Interface function for mass-spring-damper model
-        #Arguments:
-            u: Control signal value
-        #Returns:
-            Position of mass
-        """
-        #Calculates output signal
-        now = time()
-        dt = 0.0
-        if self.past is not None:
-            dt = now - self.past
-        self.past = now
-        x = odeint(self.ode, self.x0, [0.0, dt], args=(u,))
-        self.x0 = x[1,:]
-        return x[1,0]
-
-
 ##TODO: Implement this function
 #def estimate_ss(t, y, u, ns, nd=None):
 #    nu = 0.0
