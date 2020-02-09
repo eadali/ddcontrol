647fac42cfe2703eee4b50f3ef14ddef74d04c67
eadali <erkanadali91@gmail.com>
2020-02-09 18:53:30 +0300

    a

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,7 +48,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 # The master toctree document.
-master_doc = 'ddcontrol'
+master_doc = 'index'
 
 # General information about the project.
 project = 'ddcontrol'
